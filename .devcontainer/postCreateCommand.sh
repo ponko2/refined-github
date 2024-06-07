14877678e8497553de4925095e708958c44d9381
Takahito Nakano <kano@ponko2.jp>
2024-06-07 19:18:03 +0900

    build: Update dependencies

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
-pnpm config set store-dir ~/.local/share/pnpm/store
+git config --global --add safe.directory /workspaces/refined-github
+
 npm uninstall -g pnpm
 sudo corepack enable pnpm
+corepack install --global pnpm
+pnpm config set store-dir ~/.local/share/pnpm/store
+
+pnpm install --force
